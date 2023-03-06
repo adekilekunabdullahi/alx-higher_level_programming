2c22547a11684478fdf3bd9c13b0927a730355f2
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-06 05:51:32 -0800

    project file

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 import sys
 text = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+
+
 def write_to_stderr():
     sys.stderr.write(text)
     sys.exit(1)
+
+
 write_to_stderr()
