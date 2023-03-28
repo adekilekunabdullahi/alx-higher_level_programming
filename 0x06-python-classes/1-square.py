bd03c2228d01ef21e006b02bb721d20ab906b27c
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-28 16:48:31 -0700

    1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size):
-        """Example of docstring on the __init__ method. 
+        """Example of docstring on the __init__ method.
     The __init__ method may be documented in eithe      Args:
             size (:obj:`list` of :obj:`str`): Description of `param3`.
         """
