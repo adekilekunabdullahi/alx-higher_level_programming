d9c969ec3c4ef1bce40acca3d94d2560636d85f5
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-28 17:17:33 -0700

    ptoject file

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+""" This code show
+classes
+implementation in python
+"""
+
+
 class Square:
+    """a class Square that defines a square by: (based on 1-square.py)
+    """
     def __init__(self, size=0):
+        """
+        size: parameter
+        """
         self.__size = size
         if type(self.__size) != int:
             raise TypeError("size must be an integer")
