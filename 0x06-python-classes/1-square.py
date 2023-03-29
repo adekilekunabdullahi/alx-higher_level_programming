fceb136d95fdd0bdb3c5442b9377b7e35c6baaad
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-03-28 17:12:41 -0700

    project

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+""" This code demonstrate
+how to implement
+classes
+in
+python
+"""
+
+
 class Square:
-    """Documentation
+    """ a class Square that defines a square by: (based on 0-square.py)
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
     """
     def __init__(self, size):
         """Example of docstring on the __init__ method.
