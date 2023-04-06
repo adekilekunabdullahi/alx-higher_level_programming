009576d443a44d972a47bce0f395d6775af4713d
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-04-06 08:03:39 -0700

    code refactor

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -14,7 +14,10 @@ def matrix_divided(matrix, div):
         div = 10
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if type(matrix) is not list or (len(matrix) == 0)  or type(matrix[0]) is not list or (len(matrix[0]) == 0):
+    if type(matrix) is not list or (len(matrix) == 0):
+        raise TypeError("""matrix must be a matrix\
+ (list of lists) of integers/floats""")
+    if type(matrix[0]) is not list or (len(matrix[0]) == 0):
         raise TypeError("""matrix must be a matrix\
  (list of lists) of integers/floats""")
     i = 0
