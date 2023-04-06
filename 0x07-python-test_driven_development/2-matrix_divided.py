c618631ae0d51955c4fe32c8370966c6d33e4187
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-04-06 07:25:23 -0700

    updates

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -12,6 +12,9 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if type(matrix) is not list or (len(matrix) == 0)  or type(matrix[0]) is not list or (len(matrix[0]) == 0):
+        raise TypeError("""matrix must be a matrix\
+ (list of lists) of integers/floats""")
     i = 0
     new_list = list()
     length = len(matrix[0])
@@ -21,7 +24,7 @@ def matrix_divided(matrix, div):
         new_list_1 = []
         for y in x:
             if type(y) != float and type(y) != int:
-                raise TypeError("""matrix must be a matrix
+                raise TypeError("""matrix must be a matrix\
  (list of lists) of integers/floats""")
             a = y / div
             new_list_1.append(round(a, 2))
