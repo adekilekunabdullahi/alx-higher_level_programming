67222a548e536c6245bc5a3f1f258ddfa26f4f21
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-04-11 12:04:59 -0700

    files

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -6,7 +6,7 @@ class BaseGeometry:
     """Doc"""
     def area(self):
         """Doc"""
-        raise NotImplementedError("area() is not implemented")
+        raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
         """Doc"""
