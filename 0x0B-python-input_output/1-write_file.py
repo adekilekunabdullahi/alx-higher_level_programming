c43664dd409c06e462128b246b7e7f8a3f968f5d
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-04-12 07:20:34 -0700

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,4 +5,5 @@
 def write_file(filename="", text=""):
     """Doc"""
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(text)
+        num = f.write(text)
+        return len(num)
