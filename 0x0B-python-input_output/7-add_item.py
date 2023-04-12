3c2a685030abc6d5d406a861022e0851e1cfbe40
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-04-12 09:14:40 -0700

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,15 +2,19 @@
 """Doc"""
 import json
 from sys import argv
-
+from pathlib import Path
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 filename = "add_item.json"
 lists = []
-text = load_from_json_file(filename)
-for x in text:
-    lists.append(x)
+par = Path('./add_item.json')
+if par.is_file():
+    text = load_from_json_file(filename)
+    for x in text:
+        lists.append(x)
+else:
+    pass
 for x in range(len(argv)):
     if x == 0:
         continue
