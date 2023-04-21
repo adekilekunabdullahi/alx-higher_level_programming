66b00ec5bd628457bf2823623ddbedcb1bd3709c
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-04-21 04:12:04 -0700

    models/square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """Python sc"""
-from . import rectangle
+from models import rectangle
 
 
 class Square(rectangle.Rectangle):
     """Doc"""
+
     def __init__(self, size, x=0, y=0, id=None):
         """Doc"""
         super().__init__(size, size, x, y, id)
