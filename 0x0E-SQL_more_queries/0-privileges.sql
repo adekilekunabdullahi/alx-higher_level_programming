ccb5bebb515a6aa2bc8c88f91415d9fe4d1803bb
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-08-17 12:54:31 -0700

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,3 @@
 -- sql scripts to show the grants on users user_0d_1 and user_0d_2
--- sql commands
-SHOW GRANTS FOR user_0d_1@localhost;
-SHOW GRANTS FOR user_0d_2@localhost;
-
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
