8b8b4eb1073a339ccfe7a87a6852c42bdafc87b2
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-10-17 22:43:18 -0700

    project files

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- jkk
+-- jkk
+SELECT score, COUNT(*) AS number FROM second_table GROUP BY score ORDER BY score DESC;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,3 @@
+-- lists all records with a name value
+-- code to use
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
