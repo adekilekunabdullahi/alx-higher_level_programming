e102d313446fef700d92b567de284c89e821089f
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-10-17 20:03:01 -0700

    4-first_table.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,8 @@
+-- script to create a table
+-- create database
+-- CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
+-- code to use
+CREATE TABLE IF NOT EXISTS first_table (
+	id INT,
+	name VARCHAR(256)
+);
