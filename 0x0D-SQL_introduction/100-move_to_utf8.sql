0e98528d20b2fd08379e81986f5c45df4274b42a
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-10-19 01:15:32 -0700

    100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -4,5 +4,6 @@ ALTER DATABASE hbtn_0c_0
   COLLATE utf8mb4_unicode_ci;
 
 -- Change the character set and collation for the 'name' field in 'first_table'
+USE hbtn_0c_0;
 ALTER TABLE first_table
   MODIFY name VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
