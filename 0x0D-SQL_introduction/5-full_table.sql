a38e9059bc75f77fd75228410c5b51abccadbd1b
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-10-19 01:11:43 -0700

    5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,3 @@
 -- script to create a table
 -- code to use
-SELECT column_name, data_type, character_maximum_length, is_nullable
-FROM information_schema.columns
-WHERE table_name = 'first_table';
-
+SHOW CREATE TABLE first_table;
