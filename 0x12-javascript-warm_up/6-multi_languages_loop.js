ee2b66f4332b6e821d548acb8bc92e1fc0bb9abc
adekilekunabdullahi <adekilekunabdullahi@gmail.com>
2023-11-13 22:12:44 +0000

    projects

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const arrays = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let a=0; a < 3; a++) {
-console.log(arrays[a]);
-};
+for (let a = 0; a < 3; a++) {
+  console.log(arrays[a]);
+}
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-num = process.argv[2];
-if (isNaN(parseInt(num))) {
-console.log('Missing number of occurrences');}
-else if (num === undefined)
-{ console.log('Missing number of occurrences');}
-else {
-for (let a = 0; a < parseInt(num); a++) {
-	console.log('C is fun');
-}
+const num = process.argv[2];
+if (!isNaN(parseInt(num))) {
+  for (let a = 0; a < parseInt(num); a++) {
+    console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
 }
